06bee2647970b513442bceaada3a1e0dff3389e6
Danboi <168639806+Danboi01@users.noreply.github.com>
2025-02-17 19:35:07 +1300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,50 +1,63 @@
 Citizen.CreateThread(function()
     while true do
-        if Config.Enabled then
-            for _, message in pairs(Config.Messages) do
+        if Config.Enabled and #Config.Messages > 0 then
+            for _, message in ipairs(Config.Messages) do
                 SendNoti(-1, message, 'success')
-                Citizen.Wait(Config.Time * 60000)
+                Citizen.Wait(Config.Time * 60000)  
             end
+        else
+            Citizen.Wait(5000)  
         end
-        Citizen.Wait(1000) 
     end
 end)
 
 AddEventHandler('playerJoining', function()
     local playerid = source
     local name = GetPlayerName(playerid)
-    if Config.JoinEnabled then
+    if Config.JoinEnabled and Config.JoinMessage ~= "" then
         SendNoti(playerid, name .. " " .. Config.JoinMessage, 'success')
     end
 end)
 
 function SendNoti(recipient, message, type)
+    if not message or message == "" then return end 
+
     local messageType, color
     if type == "success" then 
         messageType = "SYSTEM"
         color = "~g~"
     elseif type == "error" then 
-        messageType = "SYSTEM"
+        messageType = "ERROR"
         color = "~r~"
+    else
+        messageType = "INFO"
+        color = "~b~"
     end
-    message = color .. "[" .. messageType .. "] ~w~" .. message
+
+    local formattedMessage = string.format("%s[%s] ~w~%s", color, messageType, message)
+
     if Config.Notify == 0 then
-        TriggerClientEvent('chat:addMessage', -1, {
-            template = '<div class="chat-message ooc"><b><span style="color: #7d7d7d">{1}: </span>&nbsp;<span style="font-size: 14px; color: #e1e1e1;"></span></b><div style="margin-top: 5px; font-weight: 300;"></div></div>',
-            args = { recipient, message }
+        TriggerClientEvent('chat:addMessage', recipient, {
+            template = '<div class="chat-message ooc"><b><span style="color: #7d7d7d">{1}: </span>&nbsp;<span style="font-size: 14px; color: #e1e1e1;">{2}</span></b></div>',
+            args = { messageType, message }
         })
     elseif Config.Notify == 1 then
         TriggerClientEvent('okokNotify:Alert', recipient, string.upper(type), message, Config.NotifyDuration, type, true)
     elseif Config.Notify == 2 then
-        TriggerClientEvent('codem-notification', recipient, message, 8000, type == 'success' and 'check' or 'error', options)
+        TriggerClientEvent('codem-notification', recipient, message, 8000, type == 'success' and 'check' or 'error')
     elseif Config.Notify == 3 then
-        TriggerClientEvent('mythic_notify:client:SendAlert', recipient, { type = type, text = message, style = { ['background-color'] = '#000000', ['color'] = '#ffff' } })
+        TriggerClientEvent('mythic_notify:client:SendAlert', recipient, { 
+            type = type, 
+            text = message, 
+            style = { ['background-color'] = '#000000', ['color'] = '#ffff' } 
+        })
     elseif Config.Notify == 4 then
-        TriggerClientEvent('ox_lib:notify', -1, {
-            title = 'Atlas DevLabs System',
+        TriggerClientEvent('ox_lib:notify', recipient, {
+            title = Config.Prefix or 'Server Announcement',
             description = message,
             type = type,
             position = 'center-right'
         })
     end
 end
+
